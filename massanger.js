01095b0ac2dabd4f13f00f22bc2d2d68c6f5158d
majidsaadati <majidsaadati021@gmail.com>
2023-10-06 19:26:01 +0330

    add other contact, add drawer,and js drawer

---
+++ massanger.js
@@ -0,0 +1,15 @@
+const drawer = document.querySelector(#drawerMenu)
+
+
+
+function drwerMenu () {
+    drawer.addEventListener('click', e =>{
+            if (drawer.display = none){
+                drawer.display = block
+            }else
+            if(drawer.display = block){
+                drawer.display = none
+                return
+            }
+    })
+    }
